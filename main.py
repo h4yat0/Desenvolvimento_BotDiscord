17c1637d7ae8022fceaba8d8750d0a072ce8d3a7
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2021-12-02 02:52:31 +0000

    O bot funciona até então como comandos básicos

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print('hello')
+import os
+import discord
+import random
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Estou logado como {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$d20'):
+    await message.channel.send(f'Seu resultado: {random.randint(1, 20)}')
+  
+  if message.content.startswith('$video'):
+    await message.channel.send(f'Video: https://www.youtube.com/watch?v=SPTfmiYiuok')
+  
+  if message.content.startswith('$image'):
+    await message.channel.send(f'image: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkozfDxmnmovg2tDYpHHC3JG9ttFBZCGNoP-F71Efwp_JVmlVmtQH5NdyE_aULWtEG-DM&usqp=CAU')
+
+
+senha_do_bot = os.environ['TOKEN']
+
+client.run(senha_do_bot)
