48116fb2f929c46b0fe45fb307fefb88574aaa22
h4yat0 <joaovictorht00@gmail.com>
2021-12-14 18:05:17 -0300

    mais uma

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from os import environ
 import discord
 from discord.ext import commands
-from boto.s3.connection import S3Connection
 import random
 
 # -> Prefixo Definido
@@ -9,57 +8,58 @@ bot = commands.Bot(command_prefix='.')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-  print('Estou logado como {0.user}'.format(client))
+    print('Estou logado como {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
-  msg = message.content
-  if message.author == client.user:
-    return
+    msg = message.content
+    if message.author == client.user:
+        return
 
+    # ===================== Comandos de Help ============================
+    if msg.startswith('.help'):
+        await message.channel.send(f':bookmark_tabs: Veja os comandos abaixo :bookmark_tabs: \n\n')
+        await message.channel.send(f'‎‎')
+        await message.channel.send(f':small_blue_diamond: .Dados + (d2, d4, d6, d8, d10, d12, d20)')
+        await message.channel.send(f':small_blue_diamond: .video')
+        await message.channel.send(f':small_blue_diamond: .image')
+        await message.channel.send(f':small_blue_diamond: .classe')
 
-  # ===================== Comandos de Help ============================
-  if msg.startswith('.help'):
-    await message.channel.send(f':bookmark_tabs: Veja os comandos abaixo :bookmark_tabs: \n\n')
-    await message.channel.send(f'‎‎')
-    await message.channel.send(f':small_blue_diamond: .Dados + (d2, d4, d6, d8, d10, d12, d20)')
-    await message.channel.send(f':small_blue_diamond: .video')
-    await message.channel.send(f':small_blue_diamond: .image')
-    await message.channel.send(f':small_blue_diamond: .classe')
-    
-  # ===================== Comandos de Dado ============================
+    # ===================== Comandos de Dado ============================
 
-  if msg.startswith('.dado'):
-    dices = ['d2', 'd4', 'd6', 'd8', 'd10', 'd12', 'd20']
-    dice = msg.split('.dado ', 1)[1]
+    if msg.startswith('.dado'):
+        dices = ['d2', 'd4', 'd6', 'd8', 'd10', 'd12', 'd20']
+        dice = msg.split('.dado ', 1)[1]
 
-    if dice in dices:
-      dice = dice.lower()
-      dice =  int(dice.replace('d', ''))
-      await message.channel.send(f'Seu resultado: {random.randint(1, dice)}')
-    
-    else:
-      await message.channel.send('Digite um um dado entre as opções: \n\nd2  - dado de 2  lados \nd4  - dado de 4  lados\nd6  - dado de 6  lados\nd8  - dado de 8  lados\nd10 - dado de 10 lados\nd12 - dado de 12 lados\nd20 - dado de 20 lados')
+        if dice in dices:
+            dice = dice.lower()
+            dice = int(dice.replace('d', ''))
+            await message.channel.send(f'Seu resultado: {random.randint(1, dice)}')
 
-# ===================== Comandos Classes ============================
+        else:
+            await message.channel.send(
+                'Digite um um dado entre as opções: \n\nd2  - dado de 2  lados \nd4  - dado de 4  lados\nd6  - dado de 6  lados\nd8  - dado de 8  lados\nd10 - dado de 10 lados\nd12 - dado de 12 lados\nd20 - dado de 20 lados')
 
-  if msg.startswith('.classe'):
-    await message.channel.send('Escolha uma classe: \n\nGuerreiro\nFeiticeiro\nLadino\nBarbáro\nBardo\nBruxo\nClérigo\nDruida\nMago\nMonge\nPaladino')
+    # ===================== Comandos Classes ============================
 
+    if msg.startswith('.classe'):
+        await message.channel.send(
+            'Escolha uma classe: \n\nGuerreiro\nFeiticeiro\nLadino\nBarbáro\nBardo\nBruxo\nClérigo\nDruida\nMago\nMonge\nPaladino')
 
-# ===================== Comandos de imagem ou vídeo ============================
+    # ===================== Comandos de imagem ou vídeo ============================
 
-  if msg.startswith('$video'):
-    await message.channel.send(f'Video: https://www.youtube.com/watch?v=SPTfmiYiuok')
-  
-  if msg.startswith('$image'):
-    await message.channel.send(f'image: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkozfDxmnmovg2tDYpHHC3JG9ttFBZCGNoP-F71Efwp_JVmlVmtQH5NdyE_aULWtEG-DM&usqp=CAU')
+    if msg.startswith('$video'):
+        await message.channel.send(f'Video: https://www.youtube.com/watch?v=SPTfmiYiuok')
+
+    if msg.startswith('$image'):
+        await message.channel.send(
+            f'image: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkozfDxmnmovg2tDYpHHC3JG9ttFBZCGNoP-F71Efwp_JVmlVmtQH5NdyE_aULWtEG-DM&usqp=CAU')
 
 
 senha_do_bot = environ.get('TOKEN')
 
 client.run(senha_do_bot)
-
-
