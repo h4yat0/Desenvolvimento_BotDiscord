4bd36585195f05fe18e32009aca12fae268270fe
h4yat0 <joaovictorht00@gmail.com>
2021-12-15 10:18:14 -0300

    update de teste da visualização do código

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from os import environ
 import discord
-from discord.ext import commands
 import random
+from os import environ
+from discord.ext import commands
+
 
 # -> Prefixo Definido
 bot = commands.Bot(command_prefix='.')
@@ -42,13 +43,16 @@ async def on_message(message):
 
         else:
             await message.channel.send(
-                'Digite um um dado entre as opções: \n\nd2  - dado de 2  lados \nd4  - dado de 4  lados\nd6  - dado de 6  lados\nd8  - dado de 8  lados\nd10 - dado de 10 lados\nd12 - dado de 12 lados\nd20 - dado de 20 lados')
+                'Digite um um dado entre as opções: \n\nd2  - dado de 2  lados \nd4  - dado de 4  lados\nd6  - dado '
+                'de 6  lados\nd8  - dado de 8  lados\nd10 - dado de 10 lados\nd12 - dado de 12 lados\nd20 - dado de '
+                '20 lados')
 
     # ===================== Comandos Classes ============================
 
     if msg.startswith('.classe'):
         await message.channel.send(
-            'Escolha uma classe: \n\nGuerreiro\nFeiticeiro\nLadino\nBarbáro\nBardo\nBruxo\nClérigo\nDruida\nMago\nMonge\nPaladino')
+            'Escolha uma classe: \n\nGuerreiro\nFeiticeiro\nLadino\nBarbáro\nBardo\nBruxo\nClérigo\nDruida\nMago'
+            '\nMonge\nPaladino') 
 
     # ===================== Comandos de imagem ou vídeo ============================
 
@@ -60,6 +64,6 @@ async def on_message(message):
             f'image: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkozfDxmnmovg2tDYpHHC3JG9ttFBZCGNoP-F71Efwp_JVmlVmtQH5NdyE_aULWtEG-DM&usqp=CAU')
 
 
-senha_do_bot = environ.get('TOKEN')
+password = environ.get('TOKEN')
 
-client.run(senha_do_bot)
+client.run(password)
