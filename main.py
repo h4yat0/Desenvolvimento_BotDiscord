6608611521df1c01775a3e4442a76208153fc6ee
h4yat0 <joaovictorht00@gmail.com>
2021-12-15 11:02:18 -0300

    correção do commit anterior
    update na funcionalidade '.dado'

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import random
 from os import environ
 from discord.ext import commands
 
-
 # -> Prefixo Definido
 bot = commands.Bot(command_prefix='.')
 
@@ -21,13 +20,6 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # -> Mensagens repetitivas
-    def dados():
-        tipos_dados = 'Digite um um dado entre as opções: \n\nd2  - dado de 2  lados \nd4  - dado de 4  lados\nd6  - ' \
-                      'dado de 6  lados\nd8  - dado de 8  lados\nd10 - dado de 10 lados\nd12 - dado de 12 lados\nd20 ' \
-                      '- dado de 20 lados '
-        await message.channel.send(tipos_dados)
-
     # ===================== Comandos de Help ============================
     if msg.startswith('.help'):
         await message.channel.send(f':bookmark_tabs: Veja os comandos abaixo :bookmark_tabs: \n\n')
@@ -39,28 +31,30 @@ async def on_message(message):
 
     # ===================== Comandos de Dado ============================
     if msg.startswith('.dado'):
-        dices = ['d2', 'd4', 'd6', 'd8', 'd10', 'd12', 'd20']
+        dices = [' d2', ' d4', ' d6', ' d8', ' d10', ' d12', ' d20']
         dice = msg.split('.dado', 1)[1]
+
+        tipos_dados = 'Digite um um dado entre as opções: \n\nd2  - dado de 2  lados \nd4  - dado de 4  lados\nd6  - ' \
+                      'dado de 6  lados\nd8  - dado de 8  lados\nd10 - dado de 10 lados\nd12 - dado de 12 lados\nd20 ' \
+                      '- dado de 20 lados '
 
         if dice in dices:
             dice = dice.lower()
             dice = int(dice.replace('d', ''))
             await message.channel.send(f'Seu resultado: {random.randint(1, dice)}')
-
-        if dice.isspace():
-            dados()
-
+        elif dice.strip() == '':
+            await message.channel.send(tipos_dados)
         else:
-            dados()
+            await message.channel.send(tipos_dados)
 
     # ===================== Comandos Classes ============================
 
     if msg.startswith('.classe'):
         await message.channel.send(
             'Escolha uma classe: \n\nGuerreiro\nFeiticeiro\nLadino\nBarbáro\nBardo\nBruxo\nClérigo\nDruida\nMago'
-            '\nMonge\nPaladino') 
+            '\nMonge\nPaladino')
 
-    # ===================== Comandos de imagem ou vídeo ============================
+        # ===================== Comandos de imagem ou vídeo ============================
 
     if msg.startswith('$video'):
         await message.channel.send(f'Video: https://www.youtube.com/watch?v=SPTfmiYiuok')
@@ -68,7 +62,6 @@ async def on_message(message):
     if msg.startswith('$image'):
         await message.channel.send(
             f'image: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkozfDxmnmovg2tDYpHHC3JG9ttFBZCGNoP-F71Efwp_JVmlVmtQH5NdyE_aULWtEG-DM&usqp=CAU')
-
 
 password = environ.get('TOKEN')
 
